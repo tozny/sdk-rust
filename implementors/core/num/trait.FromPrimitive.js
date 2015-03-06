b587ca796580673f6736a4797e8f457c349579b5
Jesse Hallett <jesse@galois.com>
2015-03-06 14:16:19 -0800

    Documentation for v1.0.0

---
+++ implementors/core/num/trait.FromPrimitive.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['chrono'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='chrono/enum.Weekday.html' title='chrono::Weekday'>Weekday</a>",];
+implementors['hyper'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='struct' href='hyper/header/struct.Quality.html' title='hyper::header::Quality'>Quality</a>","<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='hyper/status/enum.StatusCode.html' title='hyper::status::StatusCode'>StatusCode</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Neg.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['openssl'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];
+implementors['chrono'] = ["<a class='stability Unstable' title='Unstable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='chrono/duration/struct.Duration.html' title='chrono::duration::Duration'>Duration</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
